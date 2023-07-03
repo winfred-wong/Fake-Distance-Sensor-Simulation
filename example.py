Add mapping function and fix distance calculation bug

example.py
@@ -1,25 +1,31 @@
-import numpy as np
 from FakeDistanceSensorSimulation.config import *
 from FakeDistanceSensorSimulation.config import world_width
+from FakeDistanceSensorSimulation.helper import pol2cart
+from FakeDistanceSensorSimulation.mapping.MapManager import MapManager
 from FakeDistanceSensorSimulation.world.Border import Border
 from FakeDistanceSensorSimulation.world.Car import Car
 from FakeDistanceSensorSimulation.physics.geometry import Point, Line
 import time
 from FakeDistanceSensorSimulation.controller.KeyboardController import KeyboardController
 from FakeDistanceSensorSimulation.world.world import World
+import numpy as np
+import logging, sys
+
+# Set logging level
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # Create a world
-w = World(dt, width=world_width, height=world_height, ppm=6)
+w = World(dt, width=world_width, height=world_height, ppm=8)
 
 # Create a car and set the velocity and max/min speed
 c1 = Car(Point(world_width / 2, world_height / 2), np.pi / 2)
-c1.max_speed = 30.0
-c1.min_speed = -30.0
+c1.max_speed = 20.0
+c1.min_speed = -20.0
 c1.velocity = Point(0, 0)
 w.add(c1)
 
-# The factor used to draw the border that resembles the border line used to draw the line in fake distance sensor is different
-# This is for it to visualise the border line more accurately
+# The factor used to draw the border that resembles the border line used to draw the line in fake distance sensor is
+# different This is for it to visualise the border line more accurately
 close_side_border_visual_factor = close_side_border_factor - 0.053
 height_compensation_factor = 1.55
 i = 0
@@ -51,13 +57,25 @@ c1.set_control(0., 0.)
 # Setup controller of the keyboard
 controller = KeyboardController(w)
 
+# Init map manager that draws the map
+map_manager = MapManager()
+
 # Keep looping
 while True:
     # Set the control from the keyboard
     c1.set_control(controller.steering, controller.throttle)
 
-    # Let the world run
+    # Let the world tick
     w.tick()
+
+    # Get new coordinates from the input stream (replace this with your input stream logic)
+    new_x, new_y = pol2cart(c1.distanceToBorder, c1.heading_in_angle)
+    logging.debug(f"Cartesian coordinate of the border straight ahead from the car: {new_x}, {new_y}")
+    logging.debug(f"Car's position x and y: {c1.x}, {c1.y}")
+
+    # Update the plot
+    map_manager.update_plot(new_x + c1.x, new_y + c1.y)
+    logging.debug(f"Coordinates of detected wall of the room: {new_x + c1.x}, {new_y + c1.y}")
 
     # Render
     w.render()

fake_sensor/FakeDistanceSensor.py
@@ -1,5 +1,6 @@
-import numpy as np
+import logging
 from FakeDistanceSensorSimulation.config import *
+from FakeDistanceSensorSimulation.helper import pol2cart
 from FakeDistanceSensorSimulation.physics.geometry import Line, Point
 
 
@@ -8,56 +9,33 @@ class FakeDistanceSensor:
         self.heading_in_angle = heading_in_angle
         self.car_center = car_center
 
-    def _get_angle_normalised_under_90(self):
-        norm_angle = self.heading_in_angle
-        while norm_angle >= 90:
-            norm_angle -= 90
-        return norm_angle
-
-    def _get_quadrant(self):
-        angle = self.heading_in_angle % 360
-
-        if 0 <= angle < 90:
-            return 1
-        elif 90 <= angle < 180:
-            return 2
-        elif 180 <= angle < 270:
-            return 3
-        elif 270 <= angle < 360:
-            return 4
-        return None
-
     def _get_endpoints_of_imagined_line(self):
-        # Normalise angle and get the quadrant
-        angle = self._get_angle_normalised_under_90()
-        quadrant = self._get_quadrant()
 
         # The imaginary line has to be longer than the longest line possible within the rectangle for it to intersect
         # all possible lines
+        # 169 when 80 tall 40 wide
         imaginary_line_length = int(math.hypot(world_width, world_height) + world_height)
+        logging.debug(f"Length of imaginary line: {imaginary_line_length}")
+        logging.debug(f"Car's heading in angle: {self.heading_in_angle}")
 
-        # a and b are new x and y of the imagined line depending on which quadrant they are in
-        a = imaginary_line_length * np.sin(np.deg2rad(angle))
-        b = imaginary_line_length * np.sin(np.deg2rad(90 - angle))
+        # Convert to cartesian coordinates
+        imagined_x, imagined_y = pol2cart(imaginary_line_length, self.heading_in_angle)
 
-        # Assign the value depending on quadrant
-        line_end_y = a if quadrant % 2 else b
-        line_end_x = b if quadrant % 2 else a
+        # Convert the imagined coordinates to that with respect to origin
+        imagined_x += self.car_center.x
+        imagined_y += self.car_center.y
+        logging.debug(f"Calculated p2 x, y position of imagined line: {imagined_x}, {imagined_y}")
 
-        # Handle the negativity of the coordinate system
-        if quadrant in [2, 3]:
-            line_end_x *= -1
-
-        if quadrant in [3, 4]:
-            line_end_y *= -1
-
-        return line_end_x, line_end_y
+        # Return x and y
+        return imagined_x, imagined_y
 
     def get_sensor_value(self):
-        x, y = self._get_endpoints_of_imagined_line()
+
+        imagined_x, imagined_y = self._get_endpoints_of_imagined_line()
 
         # Draw a line between the car and the imagined point
-        car_straight_line = Line(self.car_center, Point(x, y))
+        car_straight_line = Line(self.car_center, Point(imagined_x, imagined_y))
+        logging.debug(f"Two points of the imagined straight line: {car_straight_line}")
 
         short_width = world_width * close_side_border_factor
         long_width = world_width - short_width
@@ -65,22 +43,19 @@ class FakeDistanceSensor:
         long_height = world_height - short_height
 
         # Walls for the world
-        # Bottom > left > right > top
-        for i, j in enumerate([[short_width, short_height, long_width, short_height],
-                               [short_width, short_height, short_width, long_height],
-                               [long_width, short_height, long_width, long_height],
-                               [short_width, long_height, long_width, long_height]
-                               ]):
-            if i == 0:
-                line_text = "bottom"
-            elif i == 1:
-                line_text = "left"
-            elif i == 2:
-                line_text = "right"
-            else:
-                line_text = "top"
-            line: Line = Line(Point(j[0], j[1]), Point(j[2], j[3]))
-            if car_straight_line.intersectsWith(line):
-                print(f"Distance to {line_text} line: {self.car_center.distanceTo(line)}")
-                return self.car_center.distanceTo(line)
-            i += 1
+        bottom_line: Line = Line(Point(short_width, short_height), Point(long_width, short_height))
+        left_line: Line = Line(Point(short_width, short_height), Point(short_width, long_height))
+        right_line: Line = Line(Point(long_width, short_height), Point(long_width, long_height))
+        top_line: Line = Line(Point(short_width, long_height), Point(long_width, long_height))
+
+        for line in [bottom_line, left_line, right_line, top_line]:
+            intersection_point = car_straight_line.get_intersection_point(line)
+
+            # intersects_with makes sure the intersection point is in front of the car (meaning it's facing that direction)
+            # intersection_point finds out the intersection point
+            if car_straight_line.intersects_with(line) and intersection_point:
+                logging.debug(f"Intersection point detected and its coordinates: {intersection_point}")
+                distance = self.car_center.distanceTo(intersection_point)
+                logging.debug(f"The distance from the car to the intersection point: {distance}")
+                return distance
+        return 0

physics/geometry.py
@@ -94,7 +94,7 @@ class Point:
 
         else:
             try:
-                return other.distanceTo(self)  # do we really need to try this? Does it ever succeed?
+                return other.distance_to(self)  # do we really need to try this? Does it ever succeed?
             except NameError:
                 raise NotImplementedError
             print('Something went wrong!')
@@ -108,8 +108,8 @@ point q lies on line segment 'pr'
 
 
 def onSegment(p: Point, q: Point, r: Point) -> bool:
-    return (q.x <= np.maximum(p.x, r.x) and q.x >= np.minimum(p.x, r.x) and
-            q.y <= np.maximum(p.y, r.y) and q.y >= np.minimum(p.y, r.y))
+    return (np.maximum(p.x, r.x) >= q.x >= np.minimum(p.x, r.x) and
+            np.maximum(p.y, r.y) >= q.y >= np.minimum(p.y, r.y))
 
 
 '''
@@ -136,7 +136,25 @@ class Line:
     def __str__(self):
         return 'Line(' + str(self.p1) + ', ' + str(self.p2) + ')'
 
-    def intersectsWith(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
+    def get_coefficient(self):
+        a = (self.p1.y - self.p2.y)
+        b = (self.p2.x - self.p1.x)
+        c = (self.p1.x * self.p2.y - self.p2.x * self.p1.y)
+        return a, b, -c
+
+    def get_intersection_point(self, other: ['Line']):
+        l1 = self.get_coefficient()
+        l2 = other.get_coefficient()
+        D = l1[0] * l2[1] - l1[1] * l2[0]
+        Dx = l1[2] * l2[1] - l1[1] * l2[2]
+        Dy = l1[0] * l2[2] - l1[2] * l2[0]
+        if D != 0:
+            x = Dx / D
+            y = Dy / D
+            return Point(x, y)
+        return False
+
+    def intersects_with(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
         if isinstance(other, Line):
             p1 = self.p1
             q1 = self.p2
@@ -173,7 +191,7 @@ class Line:
             if self.p1.isInside(other) or self.p2.isInside(other): return True
             E = other.edges
             for edge in E:
-                if self.intersectsWith(edge): return True
+                if self.intersects_with(edge): return True
             return False
 
         elif isinstance(other, Circle):
@@ -194,29 +212,29 @@ class Line:
         v2 = (other.p2 - other.p1)
         return v1.dot(v2)
 
-    def hasPassed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
+    def has_passed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
         p = (self.p1 + self.p2) / 2.
         return p.hasPassed(other, direction)
 
-    def distanceTo(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
+    def distance_to(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
         if isinstance(other, Point):
             return other.distanceTo(self)
 
         elif isinstance(other, Line):
-            if self.intersectsWith(other): return 0.
+            if self.intersects_with(other): return 0.
             return np.min([self.p1.distanceTo(other.p1), self.p1.distanceTo(other.p2), self.p2.distanceTo(other.p1),
                            self.p2.distanceTo(other.p2)])
 
         elif isinstance(other, Rectangle):
-            if self.intersectsWith(other): return 0.
+            if self.intersects_with(other): return 0.
             other_edges = other.edges
-            return np.min([self.distanceTo(e) for e in other_edges])
+            return np.min([self.distance_to(e) for e in other_edges])
 
         elif isinstance(other, Circle):
             return np.maximum(0, other.m.distanceTo(self) - other.r)
 
         elif isinstance(other, Ring):
-            if self.intersectsWith(other): return 0.
+            if self.intersects_with(other): return 0.
             p1m = self.p1.distanceTo(other.m)
             if p1m < other.r_inner:  # the line is inside the ring
                 p2m = self.p2.distanceTo(other.m)
@@ -249,30 +267,30 @@ class Rectangle:
     def corners(self):
         return [self.c1, self.c2, self.c3, self.c4]
 
-    def intersectsWith(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']) -> bool:
+    def intersects_with(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']) -> bool:
         if isinstance(other, Line):
-            return other.intersectsWith(self)
+            return other.intersects_with(self)
 
         elif isinstance(other, Rectangle) or isinstance(other, Circle) or isinstance(other, Ring):
             E = self.edges
             for e in E:
-                if e.intersectsWith(other): return True
+                if e.intersects_with(other): return True
             return False
 
         raise NotImplementedError
 
-    def hasPassed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
+    def has_passed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
         p = (self.c1 + self.c2 + self.c3 + self.c4) / 4.
         return p.hasPassed(other, direction)
 
-    def distanceTo(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
+    def distance_to(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
         if isinstance(other, Point) or isinstance(other, Line):
-            return other.distanceTo(self)
+            return other.distance_to(self)
 
         elif isinstance(other, Rectangle) or isinstance(other, Circle) or isinstance(other, Ring):
-            if self.intersectsWith(other): return 0.
+            if self.intersects_with(other): return 0.
             E = self.edges
-            return np.min([e.distanceTo(other) for e in E])
+            return np.min([e.distance_to(other) for e in E])
 
         raise NotImplementedError  # TODO: implement the other cases
 
@@ -285,9 +303,9 @@ class Circle:
     def __str__(self):
         return 'Circle(' + str(self.m) + ', radius = ' + str(self.r) + ')'
 
-    def intersectsWith(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
+    def intersects_with(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
         if isinstance(other, Line) or isinstance(other, Rectangle):
-            return other.intersectsWith(self)
+            return other.intersects_with(self)
 
         elif isinstance(other, Circle):
             return self.m.distanceTo(other.m) <= self.r + other.r
@@ -297,18 +315,18 @@ class Circle:
 
         raise NotImplementedError
 
-    def hasPassed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
+    def has_passed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
         return self.m.hasPassed(other, direction)
 
-    def distanceTo(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
+    def distance_to(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
         if isinstance(other, Point) or isinstance(other, Line) or isinstance(other, Rectangle):
-            return other.distanceTo(self)
+            return other.distance_to(self)
 
         elif isinstance(other, Circle):
             return np.maximum(0, self.m.distanceTo(other.m) - self.r - other.r)
 
         elif isinstance(other, Ring):
-            if self.intersectsWith(other): return 0.
+            if self.intersects_with(other): return 0.
             d = self.m.distanceTo(other.m)
             return np.maximum(other.r_inner - d, d - other.r_outer) - self.r
 
@@ -326,9 +344,9 @@ class Ring:
         return 'Ring(' + str(self.m) + ', inner radius = ' + str(self.r_inner) + ', outer radius = ' + str(
             self.r_outer) + ')'
 
-    def intersectsWith(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
+    def intersects_with(self, other: Union['Line', 'Rectangle', 'Circle', 'Ring']):
         if isinstance(other, Line) or isinstance(other, Rectangle) or isinstance(other, Circle):
-            return other.intersectsWith(self)
+            return other.intersects_with(self)
 
         elif isinstance(other, Ring):
             d = self.m.distanceTo(other.m)
@@ -339,13 +357,13 @@ class Ring:
 
         raise NotImplementedError
 
-    def hasPassed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
+    def has_passed(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring'], direction: Point) -> bool:
         return self.m.hasPassed(other, direction)
 
-    def distanceTo(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
+    def distance_to(self, other: Union['Point', 'Line', 'Rectangle', 'Circle', 'Ring']) -> float:
         if isinstance(other, Point) or isinstance(other, Line) or isinstance(other, Rectangle) or isinstance(other,
                                                                                                              Circle):
-            return other.distanceTo(self)
+            return other.distance_to(self)
 
         if isinstance(other, Ring):
             if d > self.r_outer + other.r_outer: return d - self.r_outer - other.r_outer  # rings are far away